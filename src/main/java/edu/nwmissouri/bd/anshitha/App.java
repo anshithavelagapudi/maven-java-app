1ec068d2411bdc2c8dd74e06a8eba6ee5828cf2f
anshithavelagapudi <anshithavelagapudi@gmail.com>
2021-02-19 15:44:49 -0600

    Added src and pom.xml files

---
+++ src/main/java/edu/nwmissouri/bd/anshitha/App.java
@@ -0,0 +1,13 @@
+package edu.nwmissouri.bd.anshitha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
